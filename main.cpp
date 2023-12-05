52c2351d66f362b56819fb638949f4b2b74527c5
gaetan.decarvalho@outlook.fr <gaetan.decarvalho@outlook.fr>
2023-12-05 10:37:28 +0100

    Une bonne journée de travail

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <iostream>
+#include <stdio.h>
+#include <signal.h>
 
 #include "server/server.hpp"
 
+void    exitFunction(int sig){
+    if (sig == SIGINT)
+        std::cerr << "Thanks for using IRC SERVER ! See you next time !" << std::endl;
+    return ;
+}
 int main(int argc, char **argv) {
+    signal(SIGINT, exitFunction);
     if (argc != 3) {
         std::cerr << "please enter the port value and the password" << std::endl;
         return 1;
